f949ca5f7efd602179869bc15c78bc81f6b88db0
mohammad aldere <93014363+mohammadaldere@users.noreply.github.com>
2023-01-31 01:17:19 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const api="https://www.breakingbadapi.com/api/characters";
+
+async function getData(){
+
+   try{const response=await fetch(api)
+    const data=await response.json()
+    printData(data)
+   }catch(e){
+        console.log("Error:" ,e.message)
+   }
+
+}
+
+
+function printData(data){
+    const header=document.querySelector("#header");
+    const content=document.querySelector("#content");
+    header.innerHTML +=
+     ` 
+       <select class="form-control" onchange ="getCh(this.value)">
+       <option>please select</option>
+       ${ data.map(character => ` <option>${character.name}</option> `)}
+       </select>
+    `
+    
+}
+ async function getCh(name){
+    
+     if(name !== 'please select'){
+        const response = await fetch(`${api}?name=${name}`)
+     
+      const data = await response.json()
+    content.innerHTML=  ` 
+    <h2>${data[0].name}(${data[0].nickname})</h2>
+    <h4>${data[0].portrayed}</h4>
+    <img src="${data[0].img}" width="250">
+    `
+
+     }
+}
+
+getData()
